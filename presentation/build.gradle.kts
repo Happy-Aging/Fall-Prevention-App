e16682dbea525be9ad03112dd8141d6f47bdf759
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-27 08:55:46 +0900

    [Chore]: 패키지 번들 수정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -51,20 +51,17 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.android.compose)
+    implementation(libs.bundles.android.ktx)
 
-    implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
+
+    debugImplementation(libs.bundles.android.compose.debug)
 }
