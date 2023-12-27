b68bab7e27c03dac6c817eaedc37910977524d34
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-27 14:43:59 +0900

    [Feat]: DefaultLayout 구현

--- presentation/src/main/kotlin/com/appname/happyAging/presentation/common/constant/Colors.kt
+++ presentation/src/main/kotlin/com/appname/happyAging/presentation/common/constant/Colors.kt
@@ -5,4 +5,6 @@ import androidx.compose.ui.graphics.Color
 object Colors {
     val WHITE = Color(0xFFFFFFFF)
     val BLACK = Color(0xFF000000)
+    val DIVIDER_GREY = Color(0x1F000000)
+    val BACKGROUND_COLOR = Color(0xFFFFFFFF)
 }
