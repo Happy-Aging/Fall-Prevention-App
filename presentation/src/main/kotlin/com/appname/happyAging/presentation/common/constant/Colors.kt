97332668a3a8efc3dd8d7a95240cfe24fadc51c4
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-28 10:47:27 +0900

    [Feat]: const : 아이콘, 텍스트스타일, 간격 추가

--- presentation/src/main/kotlin/com/appname/happyAging/presentation/common/constant/Colors.kt
+++ presentation/src/main/kotlin/com/appname/happyAging/presentation/common/constant/Colors.kt
@@ -10,4 +10,5 @@ object Colors {
     val KAKAO_YELLOW = Color(0xFFFFE000)
 
     val GREY_TEXT = Color(0xFFA7A7A7)
+    val TEXT_FIELD_GREY = Color(0xFFD9D9D9)
 }
--- presentation/src/main/kotlin/com/appname/happyAging/presentation/common/constant/Sizes.kt
+++ presentation/src/main/kotlin/com/appname/happyAging/presentation/common/constant/Sizes.kt
@@ -4,6 +4,10 @@ import androidx.compose.ui.unit.dp
 
 object Sizes {
     val DEFAULT_PADDING = 22.dp
+    val INTERVAL_LARGE1 = 60.dp
+    val INTERVAL_LARGE2 = 40.dp
+    val INTERVAL_LARGE3 = 30.dp
+    val INTERVAL_LARGE4 = 20.dp
     val INTERVAL1 = 8.dp
     val INTERVAL2 = 6.dp
     val INTERVAL3 = 4.dp
