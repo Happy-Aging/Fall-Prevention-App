8a7d371f3af548bae05d454545d7b899dcdb03d0
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-02 12:59:49 +0900

    [Feat]: auth-usecase 로직 변경

--- domain/src/main/kotlin/com/appname/happyAging/domain/usecase/auth/LoginUseCase.kt
+++ domain/src/main/kotlin/com/appname/happyAging/domain/usecase/auth/LoginUseCase.kt
@@ -2,14 +2,17 @@ package com.appname.happyAging.domain.usecase.auth
 
 import com.appname.happyAging.domain.model.auth.JwtToken
 import com.appname.happyAging.domain.repository.auth.AuthRepository
+import com.appname.happyAging.domain.repository.auth.JwtTokenRepository
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository,
+    private val jwtTokenRepository: JwtTokenRepository,
 ) {
-    suspend operator fun invoke(email: String, password: String): Result<JwtToken> {
+    suspend operator fun invoke(email: String, password: String): Result<Unit> {
         return runCatching {
-            authRepository.login(email, password)
+            val token = authRepository.login(email, password)
+            jwtTokenRepository.saveJwtToken(token)
         }
     }
 
--- domain/src/main/kotlin/com/appname/happyAging/domain/usecase/auth/SignupUseCase.kt
+++ domain/src/main/kotlin/com/appname/happyAging/domain/usecase/auth/SignupUseCase.kt
@@ -1,17 +1,18 @@
 package com.appname.happyAging.domain.usecase.auth
 
-import com.appname.happyAging.domain.model.auth.JwtToken
 import com.appname.happyAging.domain.repository.auth.AuthRepository
+import com.appname.happyAging.domain.repository.auth.JwtTokenRepository
 import javax.inject.Inject
 
 class SignupUseCase @Inject constructor(
     private val authRepository: AuthRepository,
+    private val jwtTokenRepository: JwtTokenRepository,
 ) {
-    suspend operator fun invoke(email: String, password: String, nickname: String): Result<JwtToken>{
+    suspend operator fun invoke(email: String, password: String, nickname: String): Result<Unit>{
         return runCatching {
             authRepository.signup(email, password, nickname)
             val jwtToken = authRepository.login(email, password)
-            jwtToken
+            jwtTokenRepository.saveJwtToken(jwtToken)
         }
     }
 }
