62f39438cd4ddde57156b124b8cd8291be6e39a0
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-02 10:56:00 +0900

    [Feat]: TextField label 추가

--- presentation/src/main/kotlin/com/appname/happyAging/presentation/common/component/CustomTextField.kt
+++ presentation/src/main/kotlin/com/appname/happyAging/presentation/common/component/CustomTextField.kt
@@ -3,6 +3,7 @@ package com.appname.happyAging.presentation.common.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,10 +14,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,6 +38,7 @@ fun CustomTextEditField(
     ),
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     value: String,
+    label: String="",
     onValueChange: (String) -> Unit
 ) {
     Row(
@@ -58,7 +64,20 @@ fun CustomTextEditField(
             value = value,
             onValueChange = onValueChange,
             keyboardActions = keyboardActions
-        )
+        ){inner ->
+            if(value.isEmpty()){
+                Text(
+                    text = label,
+                    style = TextStyles.CONTENT_TEXT3_STYLE.copy(
+                        color = Colors.GREY_TEXT
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = Sizes.INTERVAL1),
+                )
+            }
+            inner()
+        }
     }
 }
 
@@ -91,7 +110,7 @@ fun CustomTextFieldWithTitle(
 }
 
 @Composable
-@Preview
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 fun PreviewCustomTextEditField() {
-    CustomTextEditField(value = "dasdsa", onValueChange = {})
+    CustomTextEditField(value = "", onValueChange = {})
 }
