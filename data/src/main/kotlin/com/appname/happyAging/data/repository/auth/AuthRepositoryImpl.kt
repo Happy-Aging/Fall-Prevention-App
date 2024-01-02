e4259f88be66d7a7a3d97c3f7e9c2b94b2e88bc3
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-02 12:34:24 +0900

    [Fix]: singleton 미부착 fix

--- data/src/main/kotlin/com/appname/happyAging/data/repository/auth/AuthRepositoryImpl.kt
+++ data/src/main/kotlin/com/appname/happyAging/data/repository/auth/AuthRepositoryImpl.kt
@@ -3,7 +3,9 @@ package com.appname.happyAging.data.repository.auth
 import com.appname.happyAging.domain.model.auth.JwtToken
 import com.appname.happyAging.domain.repository.auth.AuthRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AuthRepositoryImpl @Inject constructor() : AuthRepository {
     override suspend fun login(email: String, password: String): JwtToken {
         return JwtToken("accessToken", "refreshToken")
