5fbb6fc2ccc7eeae6f461cde50888b66853cc381
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-05 01:35:58 +0900

    [Feat]: api retrofit 작성

--- data/src/main/kotlin/com/appname/happyAging/data/api/ApiService.kt
+++ data/src/main/kotlin/com/appname/happyAging/data/api/ApiService.kt
@@ -1,6 +1,60 @@
 package com.appname.happyAging.data.api
 
+import com.appname.happyAging.data.dto.auth.request.LoginRequest
+import com.appname.happyAging.data.dto.auth.request.SignupRequest
+import com.appname.happyAging.data.dto.auth.request.SocialLoginRequest
+import com.appname.happyAging.data.dto.auth.response.TokenResponse
+import com.appname.happyAging.data.dto.senior.request.CreateSeniorRequest
+import com.appname.happyAging.data.dto.senior.request.UpdateSeniorRequest
+import com.appname.happyAging.data.dto.senior.response.SeniorDto
+import com.appname.happyAging.data.dto.user.request.UpdateUserRequest
+import com.appname.happyAging.data.dto.user.response.UserResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
 interface ApiService {
+    //--------------------- Auth ---------------------//
+    @POST("/auth/login")
+    @Headers("Auth: false")
+    suspend fun login(@Body request: LoginRequest): TokenResponse
+
+    @POST("/auth/login/social")
+    @Headers("Auth: false")
+    suspend fun loginSocial(@Body request: SocialLoginRequest): TokenResponse
+
+    @POST("/auth/join")
+    @Headers("Auth: false")
+    suspend fun join(@Body request: SignupRequest)
+
+
+    //--------------------- User ---------------------//
+    @GET("/user")
+    suspend fun getUserList(): UserResponse
+
+    @PUT("/user")
+    suspend fun updateUser(@Body request: UpdateUserRequest)
+
+    @DELETE("/user")
+    suspend fun deleteUser()
+
+
+    //--------------------- Senior ---------------------//
+    @GET("/senior")
+    suspend fun getSeniorList(): List<SeniorDto>
+
+    @POST("/senior")
+    suspend fun createSenior(@Body request: CreateSeniorRequest): Long
+
+    @PUT("/senior/{id}")
+    suspend fun updateSenior(@Path("id") id: Long, @Body request: UpdateSeniorRequest)
+
+    @DELETE("/senior/{id}")
+    suspend fun deleteSenior(@Path("id") id: Long)
 }
 
 
