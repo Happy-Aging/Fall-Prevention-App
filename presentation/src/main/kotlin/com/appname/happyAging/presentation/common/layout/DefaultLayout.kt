4366eb7b46dd55638e1e28b17817b80ec8cf8043
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-01-08 09:42:15 +0900

    [Feat]: DefaultLayout 뒤로가기 추가

--- presentation/src/main/kotlin/com/appname/happyAging/presentation/common/layout/DefaultLayout.kt
+++ presentation/src/main/kotlin/com/appname/happyAging/presentation/common/layout/DefaultLayout.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.appname.happyAging.presentation.common.constant.Colors
 import com.appname.happyAging.presentation.common.constant.TextStyles.TITLE_LARGE2
+import com.appname.happyAging.presentation.common.utils.noRippleClickable
 
 
 /**
@@ -30,6 +34,7 @@ import com.appname.happyAging.presentation.common.constant.TextStyles.TITLE_LARG
 @Composable
 fun DefaultLayout(
     title: String? = null,
+    backButtonAction : (() -> Unit)? = null,
     actions: @Composable RowScope.() -> Unit = {},
     body: @Composable () -> Unit,
 ) {
@@ -40,20 +45,33 @@ fun DefaultLayout(
                     title = {
                         Text(text = title, style = TITLE_LARGE2)
                     },
+                    navigationIcon = {
+                        backButtonAction?.let {
+                            Icon(
+                                modifier = Modifier.noRippleClickable {
+                                    backButtonAction()
+                                },
+                                imageVector = Icons.Default.KeyboardArrowLeft,
+                                contentDescription = null,
+                            )
+                        }
+                    },
                     colors = TopAppBarDefaults.mediumTopAppBarColors(
                         containerColor = Color.White,
                     ),
-                    modifier = Modifier.background(
-                        color = Color.Gray,
-                    ).drawWithContent {
-                        drawContent()
-                        drawLine(
-                            color = Colors.DIVIDER_GREY,
-                            strokeWidth = 1.dp.toPx(),
-                            start = Offset(0f, size.height),
-                            end = Offset(size.width, size.height)
+                    modifier = Modifier
+                        .background(
+                            color = Color.Gray,
                         )
-                    },
+                        .drawWithContent {
+                            drawContent()
+                            drawLine(
+                                color = Colors.DIVIDER_GREY,
+                                strokeWidth = 1.dp.toPx(),
+                                start = Offset(0f, size.height),
+                                end = Offset(size.width, size.height)
+                            )
+                        },
                     actions = actions
                 )
             }
@@ -61,7 +79,8 @@ fun DefaultLayout(
     ) {
         Box(modifier = Modifier
             .padding(it)
-            .fillMaxSize().background(Colors.BACKGROUND_COLOR)
+            .fillMaxSize()
+            .background(Colors.BACKGROUND_COLOR)
         ) {
             body()
         }
